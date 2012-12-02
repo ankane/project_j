1f892d9dc04eb9f5aa30ab57d8869fc711b95315
Andrew Kane <acekane1@gmail.com>
2012-12-02 06:51:30 -0800

    Code cleanup

--- add_metadata.rb
+++ add_metadata.rb
@@ -5,19 +5,9 @@ require "json"
 require "time"
 
 api_photo = JSON.parse(File.read("photo.json"))
-api_image = api_photo["images"].max_by{|image| image["width"] }
-
+big_image = api_photo["images"].max_by{|image| image["width"] }
 tags = api_photo["tags"]["data"]
-
 comments = api_photo["comments"]["data"]
-comments.each do |comment|
-  # puts comment["from"]["name"]
-  # puts comment["message"]
-  # puts Time.parse(comment["created_time"])
-  # puts
-end
-
-# save to file
 
 def read_data
   str = %x[exiv2 -eX photo.jpg && cat photo.xmp]
@@ -34,8 +24,8 @@ image.read_metadata
 image.xmp_data["Xmp.dc.description"] = api_photo["name"]
 image.xmp_data["Xmp.dc.creator"] = api_photo["from"]["name"]
 image.xmp_data["Xmp.xmp.ModifyDate"] = Time.parse(api_photo["created_time"]).iso8601
-image.xmp_data["Xmp.mwg-rs.Regions/mwg-rs:AppliedToDimensions/stDim:w"] = api_image["width"].to_s
-image.xmp_data["Xmp.mwg-rs.Regions/mwg-rs:AppliedToDimensions/stDim:h"] = api_image["height"].to_s
+image.xmp_data["Xmp.mwg-rs.Regions/mwg-rs:AppliedToDimensions/stDim:w"] = big_image["width"].to_s
+image.xmp_data["Xmp.mwg-rs.Regions/mwg-rs:AppliedToDimensions/stDim:h"] = big_image["height"].to_s
 image.xmp_data["Xmp.mwg-rs.Regions/mwg-rs:AppliedToDimensions/stDim:unit"] = "pixel"
 image.xmp_data["Xmp.mwg-rs.Regions/mwg-rs:RegionList"] = ""
 tags.each_with_index do |tag, j|
@@ -45,6 +35,12 @@ tags.each_with_index do |tag, j|
   image.xmp_data["Xmp.mwg-rs.Regions/mwg-rs:RegionList[#{i}]/mwg-rs:Area/stArea:x"] = (tag["x"] / 100.0).to_s
   image.xmp_data["Xmp.mwg-rs.Regions/mwg-rs:RegionList[#{i}]/mwg-rs:Area/stArea:y"] = (tag["y"] / 100.0).to_s
   image.xmp_data["Xmp.mwg-rs.Regions/mwg-rs:RegionList[#{i}]/mwg-rs:Area/stArea:unit"] = "normalized"
+end
+comments.each do |comment|
+  # puts comment["from"]["name"]
+  # puts comment["message"]
+  # puts Time.parse(comment["created_time"])
+  # puts
 end
 
 image.write_metadata
